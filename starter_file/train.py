859e49e0aa490cf976cfd507464ac18316e5a8a7
Aleaume <aliaume64@gmail.com>
2021-10-27 17:33:44 +0200

    First draft of train.py // Not tested

--- starter_file/train.py
+++ starter_file/train.py
@@ -8,41 +8,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
-from azureml.core import Dataset
-from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Dataset, Workspace
+
 
 #Requirements: azureml-sdk (pip install azureml-sdk)
 
-
 def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-
-    return x_df, y_df
-    #return values missing initially, solved thank to issues logged in github repo.
     
+    # Split x train, y test were "points" is dropped
+    x_df = data.to_pandas_dataframe().dropna()
+    y_df = x_df.pop("points")
+    return x_df, y_df
 
 def main():
     # Add arguments to script
@@ -52,6 +28,8 @@ def main():
     parser.add_argument('--max_iter', type=int, default=10, help="Maximum number of iterations to converge")
 
     args = parser.parse_args()
+    #More argument could be passed
+    
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
@@ -67,15 +45,16 @@ def main():
     os.makedirs('outputs',exist_ok=True)
     joblib.dump(model,'./outputs/model.joblib')
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-pathData = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+#  Create TabularDataset 
+#Retrieve current workspace
+ws = Workspace.from_config()
 
-ds = Dataset.Tabular.from_delimited_files(path=pathData)
+datastore = ws.get_default_datastore() 
+dataset = Dataset.Tabular.from_delimited_files(path=(datastore, 'data/dataset_Tuscany_Wine_prepared.csv'),
+       header='ONLY_FIRST_FILE_HAS_HEADERS')
 
-#SOURCE/HELP: https://docs.microsoft.com/en-us/python/api/azureml-core/azureml.data.dataset_factory.tabulardatasetfactory?view=azure-ml-py
 
-x, y = clean_data(ds)
+x, y = clean_data(datastore)
 
 # TODO: Split data into train and test sets.
 
