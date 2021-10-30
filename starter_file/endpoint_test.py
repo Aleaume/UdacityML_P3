fb3980338b2e5277019212ad5d8a3819b5a7a088
Aléaume <32632731+Aleaume@users.noreply.github.com>
2021-10-30 18:28:56 +0200

    Update endpoint_test.py

--- starter_file/endpoint_test.py
+++ starter_file/endpoint_test.py
@@ -1,10 +1,12 @@
-data = {
-      "vintage": 2016,
-      "variety": "Sangiovese",
-      "winery": "Casa Raia",
-      "avg winter temp": 4.2,
-      "avg spring temp": 15,
-      "avg summer temp": 27,
+data = {"data":
+	[
+		{
+			"vintage": 2016,
+			"variety": "Sangiovese",
+			"winery": "Casa Raia",
+			"avg winter temp": 4.2,
+			"avg spring temp": 15,
+			"avg summer temp": 27,
 			"avg fall temp":16,
 			"avg winter sun hour":7,
 			"avg spring hour":12,
@@ -14,4 +16,6 @@ data = {
 			"avg daily precip spring":3,
 			"avg daily precip summer":1,
 			"avg daily precip fall":2
+		}
+	]
 }
